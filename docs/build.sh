e57ba88f2baeb6d7be2e882a94ad245c60157ee8
Sven Hessenmüller <sven.hessenmueller@gmail.com>
2021-08-25 00:58:56 +0200

    updates build docs (src)

--- docs/build.sh
+++ docs/build.sh
@@ -3,7 +3,8 @@ FQFN=$(realpath $0)
 MYPATH=$(dirname $FQFN)
 PY_LIB_PATH="../venv/lib/python3.7/site-packages"
 DIR_TGT="${MYPATH}/build/"
-PYMODULES="einguteswerkzeug einguteswerkzeug.core einguteswerkzeug.helpers einguteswerkzeug.helpers.gfx einguteswerkzeug.plugins einguteswerkzeug.shell"
+
+PYMODULES="einguteswerkzeug einguteswerkzeug.core einguteswerkzeug.helpers einguteswerkzeug.helpers.gfx einguteswerkzeug.plugins einguteswerkzeug.plugins.generators einguteswerkzeug.plugins.filters einguteswerkzeug.shell"
 cd $PY_LIB_PATH
 for module in $PYMODULES; do
   pydoc3 -w $module
