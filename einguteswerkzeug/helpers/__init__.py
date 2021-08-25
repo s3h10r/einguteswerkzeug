51b5249bddce7119833bc063864a50b6ede1c230
Sven Hessenmüller <sven.hessenmueller@gmail.com>
2021-08-25 03:44:46 +0200

    updates einguteswerkzeug.helpers

--- einguteswerkzeug/helpers/__init__.py
+++ einguteswerkzeug/helpers/__init__.py
@@ -7,7 +7,9 @@ import json
 import logging
 import os
 import site
+import subprocess
 import sys
+import tempfile
 
 import exifread
 from PIL import Image
@@ -47,3 +49,56 @@ def show_error(msg):
     """
     log.critical("Error: %s" % msg)
     sys.exit(1)
+
+def editor(text=None, default_editor="vi"):
+    """
+    Open an editor (environment variable EDITOR).
+    Allows to edit (given) text like the functionality
+    that happens when doing 'git commit' on the console. 
+
+    args:
+        text (string): the text to edit
+
+    optional args:
+        default_editor (string) : editor to use if environment varibale 
+                                  EDITOR is not set
+
+    returns:
+        string: the edited text
+    """
+    fd, fname = tempfile.mkstemp()
+    with os.fdopen(fd, 'w') as f:
+        f.write(text)
+    cmd = os.environ.get('EDITOR', default_editor) + ' ' + fname
+    subprocess.call(cmd, shell=True)
+    with open(fname, 'r') as f:
+        res = f.read()
+    os.unlink(fname)
+    return res
+
+def confirm_prompt(question: str, yes_by_default = True) -> bool:
+    """
+    Shows a yes/no question.
+
+    example:
+
+        >>> reply = confirm_prompt("Are you sure?")
+        >>> print(reply)
+
+    args:
+        yes_by_default (bool) : if True just hitting return equals yes.
+
+    returns:
+        bool : True if ansered with yes. False if answerded with no.
+    """
+    replies_prompt = "y/n"
+    replies_values = ["y", "n"]
+    yes = ("y")
+    if yes_by_default: 
+        replies.add("")
+        replies_prompt = "Y/n"
+        yes = ("", "y")
+    reply = None
+    while reply not in ("", "y", "n"):
+        reply = input(f"{question} ({replies_prompt}): ").lower()
+    return (reply in yes)
